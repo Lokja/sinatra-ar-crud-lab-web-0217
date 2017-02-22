30ff23d21d774a85d6e96456c831b8d430f88c50
Devin Rieger <dev0089@gmail.com>
2017-02-21 19:55:18 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,50 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  # set :views, Proc.new { File.join(root, "../views/") }
+  #
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    Post.create(params)
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id/delete' do
+    binding.pry
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170221221934) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.string "content"
+  end
 
 end
